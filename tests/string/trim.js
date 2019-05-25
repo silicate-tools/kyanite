7868c093c10c90d44ef7e20521ecc3e59dd32838
ci-build <ci-build@dusty.codes>
2019-05-25 20:44:44 +0000

    Updates

--- tests/string/trim.js
+++
@@ -1,8 +0,0 @@
-import test from 'tape'
-import trim from '../../src/string/trim'
-
-test('trim -- Returns a trimmed string', t => {
-  t.is(trim('my brown cow   '), 'my brown cow')
-  t.is(trim('   new things   '), 'new things')
-  t.end()
-})
